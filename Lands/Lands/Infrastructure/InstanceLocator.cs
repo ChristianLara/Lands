0fce6694c89a4490742bcb4700fea0946093223d
ChristianLara <ing.christianlara@hotmail.com>
2018-06-15 12:51:18 -0500

    Initial commit

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infrastructure
+{
+
+    using ViewsModels;
+ 
+
+    class InstanceLocator
+    {
+
+        #region Properties
+
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+
+        #endregion
+    }
+}
